a665b95618d64e80a5dc6e0746c6159563d552eb
Renato Gomes <renatoo.agomes@gmail.com>
2019-08-05 17:07:40 -0300

    removendo destroy/create/update/edit do endpoint /contacts

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('contacts', 'ContactAPIController');
+Route::resource('contacts', 'ContactAPIController')->except(['update', 'destroy', 'create', 'edit']);
